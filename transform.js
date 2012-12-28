66b2d05961a503c07084c4cb5150dafdcf49901c
Tom MacWright <tom@macwright.org>
2012-12-27 21:31:51 -0500

    Support transforms

---
+++ transform.js
@@ -0,0 +1,37 @@
+function toline(gj) {
+    var features = gj.features;
+    var line = {
+        type: 'Feature',
+        geometry: {
+            type: 'LineString',
+            coordinates: []
+        }
+    };
+    for (var i = 0; i < features.length; i++) {
+        line.geometry.coordinates.push(features[i].geometry.coordinates);
+    }
+    line.properties = features[0].properties;
+    return {
+        type: 'FeatureSet',
+        features: [line]
+    };
+}
+
+function topolygon(gj) {
+    var features = gj.features;
+    var poly = {
+        type: 'Feature',
+        geometry: {
+            type: 'Polygon',
+            coordinates: [[]]
+        }
+    };
+    for (var i = 0; i < features.length; i++) {
+        poly.geometry.coordinates.push(features[i].geometry.coordinates);
+    }
+    poly.properties = features[0].properties;
+    return {
+        type: 'FeatureSet',
+        features: [poly]
+    };
+}
