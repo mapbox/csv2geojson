mapbox/csv2geojson

commit ce8eaba964254cefc25e881a8601b74b3a4ae595

add --numeric-fields option (#64)

csv2geojson
@@ -10,6 +10,7 @@ var optimist = require('optimist'),
         .describe('lon', 'the name of the longitude column')
         .describe('line', 'whether or not to output points as a LineString')
         .describe('delimiter', 'the type of delimiter')
+        .describe('numeric-fields', 'comma separated list of fields to convert to numbers')
         .default('delimiter', ',')
         .default('line', false)
         .argv;
@@ -24,6 +25,7 @@ function convert(data) {
         latfield: argv.lat,
         lonfield: argv.lon,
         delimiter: argv.delimiter
+        nemericFields: argv['numeric-fields']
     }, function(err, data) {
         if (err) console.error(JSON.stringify(err, null, 2));
         if (argv.line) data = csv2geojson.toLine(data)

index.js
@@ -103,8 +103,19 @@ function csv2geojson(x, options, callback) {
         }
     }
 
+    var numericFields = options.numericFields ? options.numericFields.split(',') : null;
+
     var parsed = (typeof x == 'string') ?
-        dsv.dsvFormat(options.delimiter).parse(x) : x;
+        dsv.dsvFormat(options.delimiter).parse(x, function (d) {
+            if (numericFields) {
+                for (var key in d) {
+                    if (key in numericFields) {
+                        d[key] = +d[key];
+                    }
+                }
+            }
+            return d;
+        }) : x;
 
     if (!parsed.length) {
         callback(null, featurecollection);

test/csv2geojson.js
@@ -235,6 +235,14 @@ describe('csv2geojson', function () {
             });
         });
 
+        it('converts numeric fields', function () {
+            csv2geojson.csv2geojson(textFile('simple.csv'), {
+                numericFields: 'name'
+            }, function (err, data) {
+                expect(data).to.eql(jsonFile('number.geojson'));
+            });
+        });
+
         it('reports bad coordinates', function () {
             csv2geojson.csv2geojson(textFile('bad_coord.csv'), function (err, data) {
                 expect(data).to.eql({

test/data/number.geojson
@@ -1,0 +1,13 @@
+{
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "name": 3
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [2, 1]
+        }
+    }]
+}

commit eda09335324098f2504011f471486db3dbeaf68e

fix bugs in numericFields feature introduced in #64 (#67)
* fix syntax error in csv2geojson bin
* fix bug breaking numericFields
* require node 6 for Array.includes

.travis.yml
@@ -1,3 +1,3 @@
 language: node_js
 node_js:
-  - "4"
+  - "6"

csv2geojson
@@ -24,8 +24,8 @@ function convert(data) {
     csv2geojson.csv2geojson(data.toString(), {
         latfield: argv.lat,
         lonfield: argv.lon,
-        delimiter: argv.delimiter
-        nemericFields: argv['numeric-fields']
+        delimiter: argv.delimiter,
+        numericFields: argv['numeric-fields']
     }, function(err, data) {
         if (err) console.error(JSON.stringify(err, null, 2));
         if (argv.line) data = csv2geojson.toLine(data)

index.js
@@ -109,7 +109,7 @@ function csv2geojson(x, options, callback) {
         dsv.dsvFormat(options.delimiter).parse(x, function (d) {
             if (numericFields) {
                 for (var key in d) {
-                    if (key in numericFields) {
+                    if (numericFields.includes(key)) {
                         d[key] = +d[key];
                     }
                 }